462a3ee458cc98d574413e7285f466669952c808
Rudi Novik <rudracer@yahoo.de>
2016-12-19 13:22:05 +0100

    fab entfernt, compiliert jetzt, aber hängt sich noch auf

--- app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
+++ app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
@@ -19,16 +19,6 @@ public class TaskListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         setActionBar ((android.widget.Toolbar) findViewById(R.id.toolbar));
     }
 
