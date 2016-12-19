d4203fd6ff4884030b4cedf7abe0c42fd8b76641
Rudi Novik <rudracer@yahoo.de>
2016-12-19 15:35:04 +0100

    TaskListAdapter weitermachen

--- app/src/main/java/com/dummies/tasks/adapter/TaskListAdapter.java
+++ app/src/main/java/com/dummies/tasks/adapter/TaskListAdapter.java
@@ -1,6 +1,12 @@
 package com.dummies.tasks.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+//import android.R;
 
 /**
  * Created by Rudi on 19.12.2016.
@@ -15,4 +21,33 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHo
             "Fünf",
             "Ah ... ah ... ah!"
     };
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        //eine neue View erstellen
+        View v = (View) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_task, parent, false);
+
+        return new ViewHolder((AdapterView) v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.titleView.setText(fakeData[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fakeData.length;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        AdapterView cardView;
+        TextView titleView;
+
+        public ViewHolder (AdapterView card) {
+            super(card);
+            cardView = card;
+            titleView = (TextView)card.findViewById(R.id.text1);
+        }
+    }
 }
