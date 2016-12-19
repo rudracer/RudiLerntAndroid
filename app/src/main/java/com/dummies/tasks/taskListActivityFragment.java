9af5ace45707dfef88bc22e74121967efd27c2e2
Rudi Novik <rudracer@yahoo.de>
2016-12-19 10:46:59 +0100

    first commit

---
+++ app/src/main/java/com/dummies/tasks/taskListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.dummies.tasks;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class taskListActivityFragment extends Fragment {
+
+    public taskListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_task_list, container, false);
+    }
+}
