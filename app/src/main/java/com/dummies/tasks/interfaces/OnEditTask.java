684d45a80ac0199c0d10977d0a5ab11b2dd517c0
Rudi Novik <rudracer@yahoo.de>
2016-12-25 00:50:31 +0100

    Interface OnEditTask

---
+++ app/src/main/java/com/dummies/tasks/interfaces/OnEditTask.java
@@ -0,0 +1,10 @@
+package com.dummies.tasks.interfaces;
+
+/**
+ * Created by Rudi on 25.12.2016.
+ */
+
+public interface OnEditTask {
+    //Wird aufgerufe, wenn der Benutzer einen Task beabeiten oder einfügen will.
+    public void editTask(long id);
+}
