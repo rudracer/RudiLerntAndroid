7021e09a9160a45ccb8370d652d4a8d383a3004a
Rudi Novik <rudracer@yahoo.de>
2016-12-24 15:40:27 +0100

    Dämliche Fehler gefixt, sodass Titel jetzt "Tasks" ist

--- app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
+++ app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +18,7 @@ public class TaskListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); //0x7f0c006d = 2131492973
-        setSupportActionBar(toolbar);
+        setActionBar(toolbar);
         //toolbar.setTitle("Hallo");
         //setActionBar ((android.widget.Toolbar) findViewById(R.id.toolbar));
     }
