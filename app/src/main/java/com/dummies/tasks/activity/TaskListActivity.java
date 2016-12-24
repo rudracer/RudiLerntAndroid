1d3e85cf1c8c91e8c0598039bc0f3ef39c445ed7
Rudi Novik <rudracer@yahoo.de>
2016-12-25 00:54:13 +0100

    TaskListActivity implementiert jetzt OnEditTask

--- app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
+++ app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
@@ -1,11 +1,13 @@
 package com.dummies.tasks.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toolbar;
 import com.dummies.tasks.R;
+import com.dummies.tasks.interfaces.OnEditTask;
 
-public class TaskListActivity extends AppCompatActivity {
+public class TaskListActivity extends AppCompatActivity implements OnEditTask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +17,13 @@ public class TaskListActivity extends AppCompatActivity {
         setActionBar(toolbar);
     }
 
+    @Override
+    public void editTask(long id) {
+        /*Wenn wir aufgefordert werden, einen Task zu bearbeiten
+        oder einzufügen, starten Sie TaskEditActivity mit der
+        ID des zu bearbeitenden Tasks.
+        */
+        startActivity(new Intent(this, TaskEditActivity.class)
+            .putExtra(TaskEditActivity.EXTRA_TASKID, id));
+    }
 }
