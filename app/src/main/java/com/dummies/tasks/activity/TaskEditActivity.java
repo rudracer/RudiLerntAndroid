bc294dbf2d1566b7a72794f4a20d982ad09dee5e
Rudi Novik <rudracer@yahoo.de>
2016-12-25 00:33:09 +0100

    Zeilen aus S. 194 oben hinzugefügt, vorhandene Zeile, die nicht im Buch vorkam, auskommentiert

--- app/src/main/java/com/dummies/tasks/activity/TaskEditActivity.java
+++ app/src/main/java/com/dummies/tasks/activity/TaskEditActivity.java
@@ -2,16 +2,20 @@ package com.dummies.tasks.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.Toolbar;
 
 import com.dummies.tasks.R;
 
 public class TaskEditActivity extends Activity {
 
+    public static final String EXTRA_TASKID = "taskId";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_edit);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setActionBar((Toolbar) findViewById(R.id.toolbar));
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
