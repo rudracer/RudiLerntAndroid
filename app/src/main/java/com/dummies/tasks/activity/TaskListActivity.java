39751bb3fdef5e8b629f47ca620fccec4858697d
Rudi Novik <rudracer@yahoo.de>
2016-12-24 15:42:13 +0100

    Unnötige Imports entfernt

--- app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
+++ app/src/main/java/com/dummies/tasks/activity/TaskListActivity.java
@@ -1,13 +1,8 @@
 package com.dummies.tasks.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.dummies.tasks.R;
 
