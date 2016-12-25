a9fbf6cae57e66dede1a7b70ab3de0a271ac27a8
Rudi Novik <rudracer@yahoo.de>
2016-12-25 15:56:17 +0100

    TaskEditFragment bis S. 202

---
+++ app/src/main/java/com/dummies/tasks/fragment/TaskEditFragment.java
@@ -0,0 +1,52 @@
+package com.dummies.tasks.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.dummies.tasks.R;
+import com.dummies.tasks.activity.TaskEditActivity;
+
+/**
+ * Created by Rudi on 25.12.2016.
+ */
+
+public class TaskEditFragment extends Fragment {
+
+    public static final String DEFAULT_FRAGMENT_TAG = "taskEditFragment";
+
+    //Views
+    View rootView;
+    EditText titleText;
+    EditText notesText;
+    ImageView imageView;
+
+    long taskId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            taskId = arguments.getLong(TaskEditActivity.EXTRA_TASKID, 0L);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_task_edit, container, false);
+
+        rootView = v.getRootView();
+        titleText = (EditText) v.findViewById(R.id.title);
+        notesText = (EditText) v.findViewById(R.id.notes);
+        imageView = (ImageView) v.findViewById(R.id.image);
+
+        return v;
+    }
+}
