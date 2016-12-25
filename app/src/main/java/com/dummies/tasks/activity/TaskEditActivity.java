2c2ce69cdaf265f54c88992f72479e60f4f89a48
Rudi Novik <rudracer@yahoo.de>
2016-12-25 17:47:55 +0100

    TaskEditActivity bis S. 205

--- app/src/main/java/com/dummies/tasks/activity/TaskEditActivity.java
+++ app/src/main/java/com/dummies/tasks/activity/TaskEditActivity.java
@@ -1,10 +1,12 @@
 package com.dummies.tasks.activity;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.Toolbar;
 
 import com.dummies.tasks.R;
+import com.dummies.tasks.fragment.TaskEditFragment;
 
 public class TaskEditActivity extends Activity {
 
@@ -15,6 +17,17 @@ public class TaskEditActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_edit);
         setActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        long id = getIntent().getLongExtra(TaskEditActivity.EXTRA_TASKID, 0L); //11
+        Fragment fragment = TaskEditFragment.newInstance(id);
+        String fragmentTag = TaskEditFragment.DEFAULT_FRAGMENT_TAG; //14
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(
+                    R.id.container,
+                    fragment,
+                    fragmentTag).commit();
+        }
     }
 
 }
